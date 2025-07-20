6333742b5bd430d4aefc96680ad8a2b0c24ce06a
Vladi <vladimir.edelshtein@securitas.com>
2025-07-20 12:51:04 +0300

    service worker refactoring

--- background.js
+++ background.js
@@ -2,9 +2,7 @@ let extensionEnabled = true;
 const teamsUrl = 'https://teams.microsoft.com/v2/';
 // https://developer.chrome.com/docs/extensions/reference/api/tabs
 
-addContextMenu();
-
-chrome.action.onClicked.addListener((activeTab) => {
+chrome.action.onClicked.addListener(activeTab => {
     // if Teams tab is active
     if (activeTab.url.startsWith(teamsUrl)) {
         enableDisableContentScript(activeTab.id);
@@ -14,20 +12,14 @@ chrome.action.onClicked.addListener((activeTab) => {
 });
 
 chrome.runtime.onInstalled.addListener(async ({reason}) => {
-    /*
-    "install" Specifies the event reason as a new installation.
-    "update" an extension update.
-    "chrome_update" Chrome update.
-    "shared_module_update" update to a shared module.
-     */
-
-    const {isOnToolbar} = await chrome.action.getUserSettings();
+    console.log('Teams Always On extension ' + reason);
     const {os, arch} = await chrome.runtime.getPlatformInfo();
-    console.log('Extension icon pinned: ' + isOnToolbar);
     console.log(`Browser Platform: ${os} ${arch}`);
 
     if (reason === 'install' || reason === 'update') {
-        addContextMenu();
+        chrome.contextMenus.create({
+            id: 'reload', title: 'Reload Extension', visible: true, contexts: ['action']
+        });
 
         const alarm = await chrome.alarms.get('wakeup-alarm');
         if (!alarm) await chrome.alarms.create('wakeup-alarm', {
@@ -36,31 +28,6 @@ chrome.runtime.onInstalled.addListener(async ({reason}) => {
         });
     }
 });
-
-function addContextMenu() {
-    chrome.contextMenus.removeAll();
-
-    chrome.contextMenus.create({
-        id: 'reload', title: 'Reload Extension', visible: true, contexts: ['action']
-    });
-
-    if (chrome.contextMenus.onClicked.hasListeners()) return;
-
-    chrome.contextMenus.onClicked.addListener(async info => {
-        console.log('Clicked on the context menu button');
-        if (info.menuItemId === 'reload') {
-            const tabs = await chrome.tabs.query({url: teamsUrl})
-            if (tabs.length > 0) tabs.forEach((tab, index) => {
-                if (index === 0) chrome.tabs.update(tab.id, { active: true, url: tabs[0].url });
-                else chrome.tabs.remove(tab.id);
-            })
-
-            chrome.runtime.reload();
-        }
-    });
-
-    console.log('added a new contextMenus listener');
-}
 
 async function enableDisableContentScript(tabId) {
     extensionEnabled = !extensionEnabled;
@@ -95,4 +62,17 @@ chrome.alarms.onAlarm.addListener(async (alarm) => {
         if (teamsTabs.length) await chrome.tabs.sendMessage(teamsTabs[0].id, {extensionEnabled});
         console.log('Sending alarm', alarm.name);
     }
+});
+
+chrome.contextMenus.onClicked.addListener(async info => {
+    if (info.menuItemId !== 'reload') return;
+
+    console.log('Clicked on the context menu button');
+    const tabs = await chrome.tabs.query({url: teamsUrl})
+    if (tabs.length > 0) tabs.forEach((tab, index) => {
+        if (index === 0) chrome.tabs.update(tab.id, { active: true, url: tabs[0].url });
+        else chrome.tabs.remove(tab.id);
+    })
+
+    chrome.runtime.reload();
 });
